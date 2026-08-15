3467eadbcb6b6fb8eaa1312833473ef13d932ed3
BloxFruitsScripters <131740181+killgaming876@users.noreply.github.com>
2026-08-15 20:50:52 +0530

    fix: load final blue optical corrections last

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './styles/red-system.css';
 import './styles/red-field.css';
 import './styles/red-only-purge.css';
 import './styles/awwwards-atmosphere.css';
+import './styles/final-optics.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
