f52d6c3a70a375519edd38fc37eab375bab2d216
BloxFruitsScripters <131740181+killgaming876@users.noreply.github.com>
2026-08-15 21:01:15 +0530

    perf: remove legacy red theme styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/globals.css';
 import './styles/landing.css';
 import './styles/cinematic-shell.css';
-import './styles/red-system.css';
-import './styles/red-field.css';
-import './styles/red-only-purge.css';
 import './styles/awwwards-atmosphere.css';
 import './styles/final-optics.css';
 
